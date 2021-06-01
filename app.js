ecc8e8e92a2f1706c276156800017152b017f28d
naveen <neonew8984@gmail.com>
2021-06-01 16:07:18 +0530

    error handling

--- app.js
+++ app.js
@@ -128,7 +128,22 @@ function transformResponse() {
 
 // ERROR HANDLING
 function errorHandling() {
-    console.log('Error Handling');
+    axios
+        .get('https://jsonplaceholder.typicode.com/todoss')
+        .then((res) => showOutput(res))
+        .catch((err) => {
+            if (err.response) {
+                // Server responded with status other than 200 range
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+            } else if (err.request) {
+                // Request was made but no response
+                console.error(err.request);
+            } else {
+                console.error(err.message);
+            }
+        });
 }
 
 // CANCEL TOKEN
